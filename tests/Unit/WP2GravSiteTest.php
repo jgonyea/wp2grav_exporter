650c41f3387cedc99376231a0b4df23ae5ca60b5
Jeremy Gonyea <jeremy.gonyea@gmail.com>
2025-11-24 17:08:19 -0500

    Adds tests for site export plugin and taxonomies

--- tests/Unit/WP2GravSiteTest.php
+++ tests/Unit/WP2GravSiteTest.php
@@ -30,23 +30,50 @@ class WP2GravSiteTest extends TestCase {
 
 		include_once $test_plugin_dir . '/plugins/wp2grav-site.php';
 
+		self::generate_custom_taxonomy();
+
 		wp2grav_export_site();
+	}
+
+	private static function generate_custom_taxonomy(): void {
+
+		// New 'Subjects' taxonomy definition.
+		$labels = array(
+			'name'      => _x( 'Subjects', 'taxonomy general name' ),
+			'menu_name' => __( 'Subjects' ),
+		);
+
+		// Register the taxonomy.
+		register_taxonomy(
+			'subjects',
+			array( 'posts' ),
+			array(
+				'hierarchical'      => true,
+				'labels'            => $labels,
+				'show_ui'           => true,
+				'show_in_rest'      => true,
+				'show_admin_column' => true,
+				'query_var'         => true,
+				'public'            => true,
+				'rewrite'           => array( 'slug' => 'subject' ),
+			)
+		);
 	}
 
 	public static function tearDownAfterClass(): void {
 		global $wp_filesystem;
-        $export_dir = WP_CONTENT_DIR . '/uploads/wp2grav-exports/user-' . gmdate( 'Ymd' ) . '/';
-        $config_dir = $export_dir . 'config/';
+		$export_dir = WP_CONTENT_DIR . '/uploads/wp2grav-exports/user-' . gmdate( 'Ymd' ) . '/';
+		$config_dir = $export_dir . 'config/';
 		$wp_filesystem->rmdir( $config_dir, true );
 	}
 
 	protected function setUp(): void {
 		$this->export_dir = WP_CONTENT_DIR . '/uploads/wp2grav-exports/user-' . gmdate( 'Ymd' ) . '/';
 		$this->site_yaml  = $this->export_dir . 'config/site.yaml';
-        $this->assertFileExists(
-            $this->site_yaml,
-            "Missing site.yaml"
-        );
+		$this->assertFileExists(
+			$this->site_yaml,
+			'Missing site.yaml'
+		);
 	}
 
 	public function testValidateSiteYaml(): void {
@@ -70,5 +97,19 @@ class WP2GravSiteTest extends TestCase {
 		// Grav site description should match WordPress blogdescription.
 		$this->assertSame( 'A local test PHPUnit site', $site_yaml['metadata']['description'] );
 	}
-}
 
+	public function testValidateSiteTaxonomy(): void {
+		$site_yaml = Yaml::parseFile( $this->site_yaml );
+
+		$this->assertArrayHasKey(
+			'taxonomies',
+			$site_yaml,
+			'Missing taxonomies'
+		);
+		$this->assertSame(
+			array( 'category', 'post_tag', 'post_format', 'subjects' ),
+			$site_yaml['taxonomies'],
+			'Incorrect site taxonomy'
+		);
+	}
+}
