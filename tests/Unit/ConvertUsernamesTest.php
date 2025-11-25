d38dba5e11c39f56762ed7bee90b4be6e5164902
Jeremy Gonyea <jeremy.gonyea@gmail.com>
2025-11-24 22:53:38 -0500

    PHPCS fixes for tests

--- tests/Unit/ConvertUsernamesTest.php
+++ tests/Unit/ConvertUsernamesTest.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Class ConvertUsernamesTest
  *
@@ -13,95 +13,118 @@ use PHPUnit\Framework\OutputError;
 class ConvertUsernamesTest extends WP_UnitTestCase {
 
 	/**
-	 * A single example test.
+	 * Convert a basic username.
+	 *
+	 * @return void
 	 */
-	    public function testConvertUsername_John_Doe() {
-        // Create a mock WP_User object for testing
+	public function testConvertUsername_John_Doe() {
+		// Mock test user.
 
-        $args = array(
-            "user_login" => "John Doe"
-        );
-        $user = $this->factory->user->create_and_get($args);
+		$args = array(
+			'user_login' => 'John Doe',
+		);
+		$user = $this->factory->user->create_and_get( $args );
 
-        // Test basic conversion
-        $this->assertEquals('john_doe', convert_username_wp_to_grav($user));
-    }
+		$this->assertEquals( 'john_doe', convert_username_wp_to_grav( $user ) );
+	}
 
-    public function testConvertUsername_John__Doe() {
-        // Create a mock WP_User object for testing
+	/**
+	 * Convert hyphenated username.
+	 *
+	 * @return void
+	 */
+	public function testConvertUsername_John__Doe() {
+		// Mock test user.
 
-        $args = array(
-            "user_login" => "John_Doe"
-        );
-        $user = $this->factory->user->create_and_get($args);
+		$args = array(
+			'user_login' => 'John-Doe',
+		);
+		$user = $this->factory->user->create_and_get( $args );
 
-        // Test basic conversion
-        $this->assertEquals('john_doe', convert_username_wp_to_grav($user));
-    }
+		$this->assertEquals( 'john-doe', convert_username_wp_to_grav( $user ) );
+	}
 
-    public function testConvertUsername_JohnsDoe() {
-        // Create a mock WP_User object for testing
+	/**
+	 * Convert username with apostrophe.
+	 *
+	 * @return void
+	 */
+	public function testConvertUsername_JohnsDoe() {
+		// Mock test user.
 
-        $args = array(
-            "user_login" => "John's Doe"
-        );
-        $user = $this->factory->user->create_and_get($args);
+		$args = array(
+			'user_login' => "John's Doe",
+		);
+		$user = $this->factory->user->create_and_get( $args );
 
-        // Test basic conversion
-        $this->assertEquals('johns_doe', convert_username_wp_to_grav($user));
-    }
+		$this->assertEquals( 'johns_doe', convert_username_wp_to_grav( $user ) );
+	}
 
-    public function testConvertUsername_JohndotDoe() {
-        // Create a mock WP_User object for testing
+	/**
+	 * Convert username with a period.
+	 *
+	 * @return void
+	 */
+	public function testConvertUsername_JohndotDoe() {
+		// Mock test user.
 
-        $args = array(
-            "user_login" => "John.Doe"
-        );
-        $user = $this->factory->user->create_and_get($args);
+		$args = array(
+			'user_login' => 'John.Doe',
+		);
+		$user = $this->factory->user->create_and_get( $args );
 
-        // Test basic conversion
-        $this->assertEquals('john_doe', convert_username_wp_to_grav($user));
-    }
+		$this->assertEquals( 'john_doe', convert_username_wp_to_grav( $user ) );
+	}
 
-    public function testConvertUsername_Jo() {
-        // Create a mock WP_User object for testing
+	/**
+	 * Convert extra-short username.
+	 *
+	 * @return void
+	 */
+	public function testConvertUsername_Jo() {
+		// Mock test user.
 
-        $args = array(
-            "user_login" => "Jo"
-        );
-        $user = $this->factory->user->create_and_get($args);
-        $id = $user->ID;
+		$args = array(
+			'user_login' => 'Jo',
+		);
+		$user = $this->factory->user->create_and_get( $args );
+		$id   = $user->ID;
 
-        // Test basic conversion
-        $this->assertEquals('jo' . $id . '_', convert_username_wp_to_grav($user));
-    }
+		$this->assertEquals( 'jo' . $id . '_', convert_username_wp_to_grav( $user ) );
+	}
 
-    public function testConvertUsername_Joj() {
-        // Create a mock WP_User object for testing
+	/**
+	 * Convert short username.
+	 *
+	 * @return void
+	 */
+	public function testConvertUsername_Joj() {
+		// Mock test user.
 
-        $args = array(
-            "user_login" => "Joj"
-        );
-        $user = $this->factory->user->create_and_get($args);
-        $id = $user->ID;
+		$args = array(
+			'user_login' => 'Joj',
+		);
+		$user = $this->factory->user->create_and_get( $args );
+		$id   = $user->ID;
 
-        // Test basic conversion
-        $this->assertEquals('joj' . $id . '__', convert_username_wp_to_grav($user, 6, 16));
-    }
+		// Test basic conversion.
+		$this->assertEquals( 'joj' . $id . '__', convert_username_wp_to_grav( $user, 6, 16 ) );
+	}
 
-    public function testConvertUsername_JohnJacobJingleheimerSchmidt() {
-        // Create a mock WP_User object for testing
+	/**
+	 * Convert long username.
+	 *
+	 * @return void
+	 */
+	public function testConvertUsername_JohnJacobJingleheimerSchmidt() {
+		// Mock test user.
 
-        $args = array(
-            "user_login" => "John Jacob Jingleheimer Schmidt"
-        );
-        $user = $this->factory->user->create_and_get($args);
-        $id = $user->ID;
+		$args = array(
+			'user_login' => 'John Jacob Jingleheimer Schmidt',
+		);
+		$user = $this->factory->user->create_and_get( $args );
+		$id   = $user->ID;
 
-        // Test basic conversion
-        $this->assertEquals('john_jacob_jing' . $id, convert_username_wp_to_grav($user));
-    }
-
-
-
+		$this->assertEquals( 'john_jacob_jing' . $id, convert_username_wp_to_grav( $user ) );
+	}
 }
