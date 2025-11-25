ec117b4e3615c23125cc9fa52f6377e9be9c58d3
Jeremy Gonyea <jeremy.gonyea@gmail.com>
2025-11-25 00:09:32 -0500

    PHPCS fixes for finding posts test

--- tests/Unit/WPGravFindPostsTest.php
+++ tests/Unit/WPGravFindPostsTest.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Class WPGravFindPostsTest
  *
@@ -12,36 +12,39 @@ use PHPUnit\Framework\OutputError;
  */
 class WPGravFindPostsTest extends WP_UnitTestCase {
 
-    public function testFindAllPosts(){
+	/**
+	 * Tests finding all types of posts, not just published.
+	 *
+	 * @return void
+	 */
+	public function testFindAllPosts() {
 
-        $published_args = array(
-            'post_title'   => 'Test Published Post',
-            'post_status'  => 'publish',
-            'post_type'    => 'post',
-        );
+		$published_args = array(
+			'post_title'  => 'Test Published Post',
+			'post_status' => 'publish',
+			'post_type'   => 'post',
+		);
 
-        $unpublished_args = array(
-            'post_title'   => 'Test Draft Post',
-            'post_status'  => 'draft',
-            'post_type'    => 'post',
-        );
+		$unpublished_args = array(
+			'post_title'  => 'Test Draft Post',
+			'post_status' => 'draft',
+			'post_type'   => 'post',
+		);
 
-        $future_date = date('Y-m-d', strtotime('+1 year'));
-        $future_args = array(
-            'post_title'   => 'Test Future Post',
-            'post_status'  => 'future',
-            'post_type'    => 'post',
-            'post_date' => $future_date,
-            'post_date_gmt' => get_gmt_from_date($future_date)
-        );
+		$future_date = gmdate( 'Y-m-d', strtotime( '+1 year' ) );
+		$future_args = array(
+			'post_title'    => 'Test Future Post',
+			'post_status'   => 'future',
+			'post_type'     => 'post',
+			'post_date'     => $future_date,
+			'post_date_gmt' => get_gmt_from_date( $future_date ),
+		);
 
-        $publishedPost = $this->factory->post->create_and_get( $published_args );
-        $unpublishedPost= $this->factory->post->create_and_get( $unpublished_args );
-        $futurePost = $this->factory->post->create_and_get( $future_args );
+		$published_post   = $this->factory->post->create_and_get( $published_args );
+		$unpublished_post = $this->factory->post->create_and_get( $unpublished_args );
+		$future_post      = $this->factory->post->create_and_get( $future_args );
 
-        $posts = wp2grav_find_posts();
-        $this->assertEquals(3, count($posts));
-    }
-
-
+		$posts = wp2grav_find_posts();
+		$this->assertEquals( 3, count( $posts ) );
+	}
 }
